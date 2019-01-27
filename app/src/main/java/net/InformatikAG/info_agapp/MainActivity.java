ee631b798b3099510def96b41fd22f6811dac10b
isiko <isihd.ko@gmail.com>
2019-01-27 15:53:07 +0100

    Adding: -Fragments for Main sites

--- app/src/main/java/net/InformatikAG/info_agapp/MainActivity.java
+++ app/src/main/java/net/InformatikAG/info_agapp/MainActivity.java
@@ -1,45 +1,49 @@
 package net.InformatikAG.info_agapp;
 
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_kalender:
-                    return true;
-                case R.id.navigation_projekts:
-                    return true;
-                case R.id.navigation_coordinators:
-                    return true;
-                case R.id.navigation_cloud:
-                    return true;
-                case R.id.navigation_settings:
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ProjektsFragment()).commit();
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.navigation_kalender:
+                            selectedFragment = new CalenderFragment();
+                            break;
+                        case R.id.navigation_projekts:
+                            selectedFragment = new ProjektsFragment();
+                            break;
+                        case R.id.navigation_coordinators:
+                            selectedFragment = new CoordinatorsFragment();
+                            break;
+                        case R.id.navigation_cloud:
+                            selectedFragment = new CloudFragment();
+                            break;
+                        case R.id.navigation_settings:
+                            selectedFragment = new SettingsFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
 }
