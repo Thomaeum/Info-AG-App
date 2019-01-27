b685c77f25be1a08c32fdfacdff84bd58f21e748
isiko <isihd.ko@gmail.com>
2019-01-27 17:14:14 +0100

    Adding Member Version of the Mein Screen, Discord Icon

--- app/src/main/java/net/InformatikAG/info_agapp/MainActivityCoordinator.java
+++ app/src/main/java/net/InformatikAG/info_agapp/MainActivityCoordinator.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivityCoordinator extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_coordinator);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
---
+++ app/src/main/java/net/InformatikAG/info_agapp/MainActivityMember.java
@@ -0,0 +1,49 @@
+package net.InformatikAG.info_agapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivityMember extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_member);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProjektsFragment()).commit();
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.navigation_kalender:
+                            selectedFragment = new CalenderFragment();
+                            break;
+                        case R.id.navigation_projekts:
+                            selectedFragment = new ProjektsFragment();
+                            break;
+                        case R.id.navigation_discord:
+                            selectedFragment = new DiscordFragment();
+                            break;
+                        case R.id.navigation_cloud:
+                            selectedFragment = new CloudFragment();
+                            break;
+                        case R.id.navigation_settings:
+                            selectedFragment = new SettingsFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
+}
